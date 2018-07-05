d81b554f5462c6e742bf97a3504203ff14d1bff3
DoccyB <tjbergquist@gmail.com>
2018-07-05 17:55:12 +0100

    Added comments and removed bug

--- routingproject/routingproject.py
+++ routingproject/routingproject.py
@@ -1,9 +1,3 @@
-
-# [[58, 83, 54, 74, 45], [93, 70, 89, 25, 70], [54, 54, 87, 77, 1], [1, 36, 17, 74, 57], [76, 43, 67, 27, 81]]
-#gridFile = open("grid.py", "r")
-#grid = gridFile.read()
-#gridFile.close()
-
 #  0  1  2  3  4 (column numbers)
 #  -  -  -  -  -
 # 58 93 54  1 76 | row 0
@@ -12,7 +6,7 @@
 # 74 25 77 74 27
 # 45 70  1 57 81 | row 4
 
-# generate grid, with each sub-list as column (155?)
+# generate grid, with each sub-list as column (answer = 155)
 grid = [[58, 83, 54, 74, 45],
 	[93, 70, 89, 25, 70],
 	[54, 54, 87, 77,  1],
@@ -22,24 +16,23 @@ grid = [[58, 83, 54, 74, 45],
 
 
 def cycleThrough(grid):
-	# index for current row
+	# index for current row and column
 	rowIndex = 0
-
 	currentColumn = len(grid)-2
-
-	# will replace workingArray each time a column is cycled through
-	# working array = last column (to begin with)
+	# working array = last column (for the first cycle)
 	workingArray = grid[len(grid)-1]
-
+	# will replace workingArray each time a column is cycled through
 	while currentColumn >= 0:
 		workingArray = processColumn(grid, workingArray, currentColumn)
 		currentColumn -= 1
-	print(sort(workingArray)[0])
+	# prints shortest final route
+	print(sorted(workingArray)[0])
 
 
 def processColumn(grid, workingArray, currentColumn):
-	workingArrayTemp = []
 	rowIndex = 0
+	workingArrayTemp = []
+
 	# loops through penultimate column and estblishes a working array
 	for rowValue in grid[currentColumn]:
 		additions = []
@@ -53,6 +46,9 @@ def processColumn(grid, workingArray, currentColumn):
 			additions.append(rowValue + workingArray[rowIndex-1])
 		# adds lowest total to WA and resets  additions
 		workingArrayTemp.append(sorted(additions)[0])
+
 		rowIndex += 1
 	return(workingArrayTemp)
+
 cycleThrough(grid)
+
